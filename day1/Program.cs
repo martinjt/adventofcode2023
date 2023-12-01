44b23f256df29c9782bc6f084d74713438331995
MartinDotNet <github@my2cents.co.uk>
2023-12-01 15:16:41 +0000

    day1 - puzzle 1

---
+++ day1/Program.cs
@@ -0,0 +1,30 @@
+﻿string filename = Environment.GetCommandLineArgs()[1];
+if (string.IsNullOrEmpty(filename))
+{
+    Console.WriteLine("Please provide a filename");
+    return;
+}
+
+var inputs = File.ReadAllLines(filename);
+var values = new List<int>();
+
+foreach (var input in inputs) {
+    double? first = null;
+    double? last = null;
+
+    foreach (var character in input.AsSpan()) {
+        if (char.IsDigit(character) ) {
+            var number = char.GetNumericValue(character);
+            if (first == null) {
+                first = number;
+                last = number;
+            } else {
+                last = number;
+            }
+        }
+    }
+    var output =  int.Parse($"{first}{last}");
+    values.Add(output);
+}
+
+Console.WriteLine($"Sum: {values.Sum()}");
