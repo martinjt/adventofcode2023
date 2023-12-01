0f3720d52dbaaab65568fe65772ffd505fec1d79
MartinDotNet <github@my2cents.co.uk>
2023-12-01 21:08:19 +0000

    day 1: puzzle 2 - Go

--- day1-go/main.go
+++ day1-go/main.go
@@ -8,6 +8,11 @@ import (
 	"unicode"
 )
 
+var numberWords = map[int]string{
+	1: "one", 2: "two", 3: "three", 4: "four",
+	5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine",
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide an input file")
@@ -24,35 +29,67 @@ func main() {
 	fileAsString := string(bytes)
 	lines := strings.Split(fileAsString, "\n")
 
-	firstOutput, err := getOutput(lines, false)
+	firstOutput, err := getOutput(lines, false, false)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	secondOutput, err := getOutput(lines, true, true)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println("First output:", firstOutput)
+	fmt.Println("Second output:", secondOutput)
 }
 
-func getOutput(lines []string, allowNumberString bool) (int, error) {
+func getOutput(lines []string, allowNumberString bool, withOutput bool) (int, error) {
 	output := 0
-	for _, line := range lines {
-		first := -1
-		last := -1
+	for lineNumber, line := range lines {
+		parsedNumber, err := parseLineForNumbers(line, allowNumberString)
+		if err != nil {
+			return -1, fmt.Errorf("error parsing line %d: %s", lineNumber, err)
+		}
+		output += parsedNumber
+	}
+	return output, nil
+}
 
-		for _, character := range line {
-			if unicode.IsDigit(character) {
-				digit := int(character)
-				last = digit
-				if first == -1 {
-					first = digit
-				}
+func parseLineForNumbers(line string, supportNumbersAsWords bool) (int, error) {
+	var first int = -1
+	var last int = -1
+
+	for characterIndex, character := range line {
+		if unicode.IsDigit(character) {
+			digit, _ := strconv.Atoi(fmt.Sprintf("%c", character))
+			last = digit
+			if first == -1 {
+				first = digit
 			}
 		}
 
-		value, err := strconv.Atoi(fmt.Sprintf("%c%c", first, last))
-		if err != nil {
-			return 0, err
+		if !supportNumbersAsWords {
+			continue
 		}
-		output += value
+
+		restOfLine := line[characterIndex:]
+		for number, word := range numberWords {
+			if strings.HasPrefix(restOfLine, word) {
+				last = number
+				if first == -1 {
+					first = number
+				}
+			}
+		}
 	}
-	return output, nil
+	if last == -1 {
+		return first, nil
+	}
+
+	stringAnswer := fmt.Sprintf("%d%d", first, last)
+	value, err := strconv.Atoi(stringAnswer)
+	if err != nil {
+		return -1, fmt.Errorf("error parsing %s: %s", stringAnswer, err)
+	}
+	return value, nil
 }
