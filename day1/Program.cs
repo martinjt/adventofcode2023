d8b88fa97634050b0c6076fe1355cda7f18ffb5f
MartinDotNet <github@my2cents.co.uk>
2023-12-01 16:04:14 +0000

    Refactoring

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,4 +1,7 @@
-﻿string filename = Environment.GetCommandLineArgs()[1];
+﻿var filename = Environment.GetCommandLineArgs().Count() != 1 ?
+    Environment.GetCommandLineArgs()[1] :
+    "";
+Console.WriteLine($"Reading from file: {filename}");
 if (string.IsNullOrEmpty(filename))
 {
     Console.WriteLine("Please provide a filename");
@@ -6,67 +9,74 @@ if (string.IsNullOrEmpty(filename))
 }
 
 var inputs = File.ReadAllLines(filename);
-var firstValues = new List<int>();
 
-foreach (var input in inputs) {
-    double? first = null;
-    double? last = null;
-
-    foreach (var character in input.AsSpan()) {
-        if (char.IsDigit(character) ) {
-            var number = char.GetNumericValue(character);
-            if (first == null) {
-                first = number;
-                last = number;
-            } else {
-                last = number;
-            }
-        }
-    }
-    var output =  int.Parse($"{first}{last}");
-    firstValues.Add(output);
+if (!inputs.Any())
+{
+    Console.WriteLine("No inputs found");
+    return;
 }
-
-var numberStrings = new [] {
+var numberStrings = new[] {
     "one", "two", "three", "four",
     "five", "six", "seven", "eight", "nine"
 };
 
-var secondValues = new List<int>();
-
-foreach (var input in inputs) {
-    double? first = null;
-    double? last = null;
-
-    for (int i = 0; i < input.Length; i++) {
-        if (char.IsDigit(input[i]) ) {
-            var number = char.GetNumericValue(input[i]);
-            if (first == null) {
-                first = number;
-                last = number;
-            } else {
-                last = number;
-            }
-            continue;
-        }
-        for (int j = 0; j < numberStrings.Length; j++) {
-            var numberString = numberStrings[j];
-            var numberValue = j + 1;
-            if (input[i..].StartsWith(numberString)) {
-                if (first == null) {
-                    first = numberValue;
-                    last = numberValue;
-                } else {
-                    last = numberValue;
-                }
-                break;
-            }
-        }
-    }
-
-    var output =  int.Parse($"{first}{last}");
-    secondValues.Add(output);
-}
+var firstValues = GetValues(inputs, false);
+var secondValues = GetValues(inputs, true);
 
 Console.WriteLine($"Puzzle 1 Sum: {firstValues.Sum()}");
 Console.WriteLine($"Puzzle 2 Sum: {secondValues.Sum()}");
+
+List<int> GetValues(string[] inputs, bool supportNumberStrings)
+{
+    var values = new List<int>();
+    foreach (var input in inputs)
+    {
+        double? first = null;
+        double? last = null;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (char.IsDigit(input[i]))
+            {
+                var number = char.GetNumericValue(input[i]);
+                if (first == null)
+                {
+                    first = number;
+                    last = number;
+                }
+                else
+                {
+                    last = number;
+                }
+                continue;
+            }
+
+            if (!supportNumberStrings)
+                continue;
+
+            for (int j = 0; j < numberStrings.Length; j++)
+            {
+                var numberString = numberStrings[j];
+                var numberValue = j + 1;
+                if (input[i..].StartsWith(numberString))
+                {
+                    if (first == null)
+                    {
+                        first = numberValue;
+                        last = numberValue;
+                    }
+                    else
+                    {
+                        last = numberValue;
+                    }
+                    break;
+                }
+            }
+        }
+
+        var output = int.Parse($"{first}{last}");
+        values.Add(output);
+    }
+    return values;
+
+}
